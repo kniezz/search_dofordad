f82bc9940551f93cca063c6390ff02be457dfcc2
Jittawat <jittawat@sandboxglobal.com>
2019-02-27 17:34:47 +0700

    build

---
+++ build/precache-manifest.019fdaac839ac8bfda58b3d183288690.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "dc4d46278bfff9575b68",
+    "url": "/static/js/main.dc4d4627.chunk.js"
+  },
+  {
+    "revision": "f39624b01d29e1627497",
+    "url": "/static/js/2.f39624b0.chunk.js"
+  },
+  {
+    "revision": "dc4d46278bfff9575b68",
+    "url": "/static/css/main.10069a34.chunk.css"
+  },
+  {
+    "revision": "7fccfcbdf3c9cd4682d05c174cb10d3b",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { data as DATA } from './data.json';
 
 class App extends Component {
+  
+  state = {
+    keyword: '',
+    filteredData: DATA
+  }
+
+  onKeywordChange = (e) => {
+    this.setState({keyword: e.target.value}, this.search);
+  }
+
+  search() {
+    const filteredData = DATA.filter(d => d.name.toLowerCase().indexOf(this.state.keyword.toLowerCase()) !== -1);
+    this.setState({ filteredData: filteredData });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input 
+          value={this.state.value}
+          type='text' 
+          placeholder='Search name...' 
+          onChange={this.onKeywordChange} />
+        <div>
+          {this.state.filteredData.length == 0? 'Not found' : this.state.filteredData.length > 100? `Please search more specific name, current result: ${this.state.filteredData.length}` :
+            this.state.filteredData.map(data => 
+              <div key={data['fb_id']} style={{ padding: '5px', border: '1px solid #000'}}>
+                <div>Name: {data['name']}</div>
+                <div>Goal: {data['goal']}</div>
+              </div>  
+            )
+          }
+        </div>
       </div>
     );
   }
